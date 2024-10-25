fbf31bcd11a76586f9ab115c15526a595a569a6c
marbitkroea <awesome@marbitkorea.com>
2024-10-25 11:01:38 +0900

    pxt.json, main.ts, main.py 업데이트하기

---
+++ main.py
@@ -0,0 +1,5 @@
+@namespace
+class screenMagic:
+    def plotAt(Index: number):
+        x = Math.round(index / 5)
+        y = Math.round(index % 5)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic {
+        
+        export function plotAt(Index:number) : void{
+            Index |= 0
+            const y = Math.floor(index / 5);
+            const x = Math.floor(index % 5);
+            led.plot(x,y)
+
+        }
+}
